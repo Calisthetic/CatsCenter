805b1a9c254872173a5a3ca076f2eb33bc059982
Calisthetic <vovakalash04@mail.ru>
2023-06-11 17:11:53 +0500

    Random

---
+++ CatsCenterAPI/Controllers/FelidsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CatsCenterAPI.Models;
+using CatsCenterAPI.Models.DataTransferObjects;
+
+namespace CatsCenterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FelidsController : ControllerBase
+    {
+        private readonly string imagesPath = @"C:\cat_images";
+        private readonly CatsCenterDbContext _context;
+
+        public FelidsController(CatsCenterDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Felids
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CatWithImage>>> GetCats(int count = 1)
+        {
+          if (_context.Cats == null)
+          {
+              return NotFound();
+          }
+            return _context.Cats.Where(x => x.Approved == true && (x.Classification == null ? false : x.Classification.IsBreed) == false).OrderBy(x => Guid.NewGuid()).Take(count)
+                .Include(x => x.Classification).Include(x => x.AddedUser).ToList().ConvertAll(x => new CatWithImage(x));
+        }
+
+        // GET: api/Felids/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cat>> GetCat(int id)
+        {
+          if (_context.Cats == null)
+          {
+              return NotFound();
+          }
+            var cat = await _context.Cats.FindAsync(id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return cat;
+        }
+
+        // PUT: api/Felids/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCat(int id, Cat cat)
+        {
+            if (id != cat.CatId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Felids
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cat>> PostCat(Cat cat)
+        {
+          if (_context.Cats == null)
+          {
+              return Problem("Entity set 'CatsCenterDbContext.Cats'  is null.");
+          }
+            _context.Cats.Add(cat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCat", new { id = cat.CatId }, cat);
+        }
+
+        // DELETE: api/Felids/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCat(int id)
+        {
+            if (_context.Cats == null)
+            {
+                return NotFound();
+            }
+            var cat = await _context.Cats.FindAsync(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cats.Remove(cat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CatExists(int id)
+        {
+            return (_context.Cats?.Any(e => e.CatId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CatsCenterAPI/Controllers/RandomController.cs
@@ -0,0 +1,49 @@
+﻿using CatsCenterAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatsCenterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly string imagesPath = @"C:\cat_images";
+        private readonly CatsCenterDbContext _context;
+
+        public RandomController(CatsCenterDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetRandomImage()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var cat = await _context.Cats.Where(x => x.Approved == true).OrderBy(x => Guid.NewGuid()).FirstOrDefaultAsync();
+
+                if (cat == null)
+                    continue;
+
+                string classificationFolder = "NoClassification";
+                if (cat.Classification != null)
+                    classificationFolder = cat.Classification.Name;
+
+                string fileType = cat.FileType.Contains("svg") ? "svg" : cat.FileType[6..];
+                string path = imagesPath + "\\" + classificationFolder + "\\" + cat.CatId + "." + fileType;
+                if (System.IO.File.Exists(path))
+                {
+                    byte[] bytes = await System.IO.File.ReadAllBytesAsync(path);
+                    return File(bytes, cat.FileType);
+                }
+                else
+                {
+                    _context.Cats.Remove(cat);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return NotFound("There's no image in collection");
+        }
+    }
+}
