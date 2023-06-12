89fd418ca4ee7eb9b75ef1650df92bbfddd360b4
Calisthetic <vovakalash04@mail.ru>
2023-06-12 20:30:31 +0500

    Comments + kitty trigger by filename

--- ConsoleScripts/Scripts/DataBase.cs
+++ ConsoleScripts/Scripts/DataBase.cs
@@ -23,22 +23,25 @@ namespace ConsoleScripts.Scripts
                 var filesFromDir = Directory.GetFiles(path);
                 foreach (var filePath in filesFromDir)
                 {
-                    if (!File.Exists(filePath))
-                        return;
-
+                    // Get classification by parent folder
                     string parentFolder = path[(path.LastIndexOf('\\') + 1)..];
                     Classification? classification = await context.Classifications.Where(x => x.Name == parentFolder).FirstOrDefaultAsync();
 
+                    // Search breed or felids from parameter
                     Classification? breed = null;
                     if (classificationId != null)
                         breed = await context.Classifications.FindAsync(classificationId);
 
+                    // Get file format, image bytes and 0 or 1 as bool for cat.IsKitty
                     string fileFormat = System.IO.Path.GetExtension(filePath) == ".jpg" ? ".jpeg" : System.IO.Path.GetExtension(filePath);
                     var bytes = await File.ReadAllBytesAsync(filePath);
+                    var kittyTrigger = Path.GetFileName(filePath).Substring(Path.GetFileName(filePath).IndexOf(".") - 1, 1);
+
                     Cat newCat = new Cat()
                     {
                         Approved = true,
-                        IsKitty = isKitty,
+                        // trying to check last letter from file name
+                        IsKitty = int.TryParse(kittyTrigger, out int amIKitty) ? (amIKitty == 1 ? true : (amIKitty == 0 ? false : isKitty)) : isKitty,
                         ClassificationId = classification == null ? (breed == null ? null : breed.ClassificationId) : classification.ClassificationId,
                         AddedUserId = 1,
                         FileType = "image/" + fileFormat[1..],
@@ -47,14 +50,17 @@ namespace ConsoleScripts.Scripts
                     await context.AddAsync(newCat);
                     await context.SaveChangesAsync();
 
+                    // Get classification folder
                     string folderName = classification == null ? (breed == null ? "NoClassification" : breed.Name) : classification.Name;
                     if (!Directory.Exists(imagesPath + "\\" + folderName))
                         Directory.CreateDirectory(imagesPath + "\\" + folderName);
 
+                    // Get new file path and copy new image to new file path
                     string newFilePath = imagesPath + "\\" + folderName + "\\" + newCat.CatId + (newCat.IsKitty ? "1" : "0") + fileFormat;
                     File.Copy(filePath, newFilePath);
                 }
 
+                // Recursion for other directories
                 var directoriesFromDir = Directory.GetDirectories(path);
                 foreach (string directory in directoriesFromDir)
                 {
